68f5a4952cb090a1f392a1ba2fc414c86823a4be
Jia Ming Luk <jiaming.luk@configura.com>
2023-06-12 21:12:54 +0800

    first commit

---
+++ app/src/main/java/com/mooddiary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mooddiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
